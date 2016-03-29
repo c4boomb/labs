6205d56aed9374d6e88df9be1bc07f8501b0c0f9
Helga-Helga <olyalavyagina@gmail.com>
2016-03-29 13:34:57 +0300

    Created DataFrames for regions

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -2,80 +2,97 @@ from spyre import server
 
 import pandas as pd
 import urllib2
-import json
+import matplotlib.pyplot as plt
+import numpy as np
 
 class StockExample(server.App):
-    title = "Inputs"
+  title = "Inputs"
 
-    inputs = [{     "type":'dropdown',
-                    "label": 'Index  ', 
-                    "options" : [ {"label": "VCI", "value":"vci"},
-                                  {"label": "TCI", "value":"tci"},
-                                  {"label": "VHI", "value":"vhi"},],
-                    "key": 'ticker', 
+  inputs = [{     "type":'dropdown',
+                  "label": 'Index  ', 
+                  "options" : [ {"label": "VCI", "value":"vci"},
+                                {"label": "TCI", "value":"tci"},
+                                {"label": "VHI", "value":"vhi"},],
+                  "key": 'index', 
+                  "action_id": "update_data"},
+
+              {     "type":'dropdown',
+                    "label": 'Region', 
+                    "options" : [ {"label": "Vinnitsya", "value":"01"},
+                                  {"label": "Volyn", "value":"02"},
+                                  {"label": "Dnipropetrovsk", "value":"03"},
+                                  {"label": "Donetsk", "value":"04"},
+                                  {"label": "Zhytomyr", "value":"05"},
+                                  {"label": "Transcarpathia", "value":"06"},
+                                  {"label": "Zaporizhzhya", "value":"07"},
+                                  {"label": "Ivano-Frankivsk", "value":"08"},
+                                  {"label": "Kiev", "value":"09"},
+                                  {"label": "Kirovohrad", "value":"10"},
+                                  {"label": "Luhansk", "value":"11"},
+                                  {"label": "Lviv", "value":"12"},
+                                  {"label": "Mykolayiv", "value":"13"},
+                                  {"label": "Odessa", "value":"14"},
+                                  {"label": "Poltava", "value":"15"},
+                                  {"label": "Rivne", "value":"16"},
+                                  {"label": "Sumy", "value":"17"},
+                                  {"label": "Ternopil", "value":"18"},
+                                  {"label": "Kharkiv", "value":"19"},
+                                  {"label": "Kherson", "value":"20"},
+                                  {"label": "Khmelnytskyy", "value":"21"},
+                                  {"label": "Cherkasy", "value":"22"},
+                                  {"label": "Chernivtsi", "value":"23"},
+                                  {"label": "Chernihiv", "value":"24"},
+                                  {"label": "Crimea", "value":"25"},
+                                  {"label": "Kiev City", "value":"26"},
+                                  {"label": "Sevastopol", "value":"27"}],
+                    "key": 'region', 
                     "action_id": "update_data"},
 
-                {     "type":'dropdown',
-                      "label": 'Region', 
-                      "options" : [ {"label": "Vinnitsya", "value":"01"},
-                                    {"label": "Volyn", "value":"02"},
-                                    {"label": "Dnipropetrovsk", "value":"03"},
-                                    {"label": "Donetsk", "value":"04"},
-                                    {"label": "Zhytomyr", "value":"05"},
-                                    {"label": "Transcarpathia", "value":"06"},
-                                    {"label": "Zaporizhzhya", "value":"07"},
-                                    {"label": "Ivano-Frankivsk", "value":"08"},
-                                    {"label": "Kiev", "value":"09"},
-                                    {"label": "Kirovohrad", "value":"10"},
-                                    {"label": "Luhansk", "value":"11"},
-                                    {"label": "Lviv", "value":"12"},
-                                    {"label": "Mykolayiv", "value":"13"},
-                                    {"label": "Odessa", "value":"14"},
-                                    {"label": "Poltava", "value":"15"},
-                                    {"label": "Rivne", "value":"16"},
-                                    {"label": "Sumy", "value":"17"},
-                                    {"label": "Ternopil", "value":"18"},
-                                    {"label": "Kharkiv", "value":"19"},
-                                    {"label": "Kherson", "value":"20"},
-                                    {"label": "Khmelnytskyy", "value":"21"},
-                                    {"label": "Cherkasy", "value":"22"},
-                                    {"label": "Chernivtsi", "value":"23"},
-                                    {"label": "Chernihiv", "value":"24"},
-                                    {"label": "Crimea", "value":"25"},
-                                    {"label": "Kiev City", "value":"26"},
-                                    {"label": "Sevastopol", "value":"27"}],
-                      "key": 'ticker', 
-                      "action_id": "update_data"},
+              { "input_type":"text",
+                "variable_name":"year",
+                "label": "Year",
+                "value":1981,
+                "key": 'year',
+                "action_id":"update_data"},
 
-                { "input_type":"text",
-                  "variable_name":"year",
-                  "label": "Year",
-                  "value":1981,
-                  "action_id":"update_data"},
+              { "type":'slider',
+                "label": 'First week', 
+                "min" : 1,"max" : 52,"value" : 35,
+                "key": 'first', 
+                "action_id": 'update_data'},
 
-                { "type":'slider',
-                  "label": 'First week', 
-                  "min" : 1,"max" : 52,"value" : 35,
-                  "key": 'first', 
-                  "action_id": 'update_data'},
+              { "type":'slider',
+                "label": 'Last week', 
+                "min" : 1,"max" : 52,"value" : 35,
+                "key": 'last', 
+                "action_id": 'update_data'},]
 
-                { "type":'slider',
-                  "label": 'Last week', 
-                  "min" : 1,"max" : 52,"value" : 35,
-                  "key": 'last', 
-                  "action_id": 'update_data'},]
+  controls = [{   "type" : "hidden",
+                  "id" : "update_data"}]
 
-    tabs = ["Plot", "Table"]
+  tabs = ["Plot", "Table"]
 
-    outputs = [{ "type" : "plot",
-                    "id" : "plot",
-                    "control_id" : "update_data",
-                    "tab" : "Plot"},
-                { "type" : "table",
-                    "id" : "table_id",
-                    "control_id" : "update_data",
-                    "tab" : "Table",
-                    "on_page_load" : True }]
+  outputs = [{ "type" : "plot",
+                  "id" : "plot",
+                  "control_id" : "update_data",
+                  "tab" : "Plot"},
+              { "type" : "table",
+                "id" : "table_id",
+                "control_id" : "update_data",
+                "tab" : "Table"}]
+
+  def getData(self, params):
+    index = params['index']
+    region = params['region']
+    year = params['year']
+    first = params['first']
+    last = params['last']
+
+    path = '/home/helga/ipt/proga/lab1/clean_data/06_03_5pm{}.csv'.format(region)
+
+    df = pd.read_csv(path, index_col=False, header=True, 
+                     names=['year', 'week', 'SMN', 'SMT', 'VCI', 'TCI', 'VHI', 'VHI<15', 'VHI<35'])
+    return df
 
 
 app = StockExample()
