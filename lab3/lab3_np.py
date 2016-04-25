83d9a2c7ba56486e7aa4b39c6a834e01d61f14fe
helha-helga <olyalavyagina@gmail.com>
2016-04-25 16:42:42 +0000

    Created array from dataframe

--- lab3/lab3_np.py
+++ lab3/lab3_np.py
@@ -1,10 +1,13 @@
 import numpy as np
+import pandas as pd
 
 def read_frame():
     path = './household_power_consumption_clean.csv'
-    my_data = np.genfromtxt(path, delimiter=';', dtype=None,
+    df = pd.read_csv(path, index_col=False, header=8, delimiter=';',
                      names=['Date', 'Time', 'Global_active_power', 'Global_reactive_power', 'Voltage', 'Global_intensity', 'Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'])
+    my_data = df.values
     print my_data
+    print my_data.shape
     return my_data
 
 read_frame()
