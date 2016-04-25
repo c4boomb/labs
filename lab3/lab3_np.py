5c034463b48b943fb164ea125a210c3df2ebe746
helha-helga <olyalavyagina@gmail.com>
2016-04-25 17:13:07 +0000

    Selected rows from numpy array where value of third column is greater than 5

--- lab3/lab3_np.py
+++ lab3/lab3_np.py
@@ -1,13 +1,17 @@
 import numpy as np
 import pandas as pd
 
-def read_frame():
+def read_array():
     path = './household_power_consumption_clean.csv'
     df = pd.read_csv(path, index_col=False, header=8, delimiter=';',
                      names=['Date', 'Time', 'Global_active_power', 'Global_reactive_power', 'Voltage', 'Global_intensity', 'Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'])
     my_data = df.values
-    print my_data
-    print my_data.shape
     return my_data
 
-read_frame()
+def active_power():
+    array = read_array()
+    print 'Households with Global_active_power more than 5 kW'
+    array = array[array[:,2] > 5]
+    print array
+
+active_power()
